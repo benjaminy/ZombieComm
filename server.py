c0fda684cd3f5d2ace8d424b82462c3cd286b732
walker <williamwalkerpollard@gmail.com>
2014-10-19 17:20:41 -0600

    record method added, NOT TESTED YET!

--- server.py
+++ server.py
@@ -1,1 +1,33 @@
+import pyaudio
+import collections
+
+
+'''
+ZombieComm radio server
+'''
+
+sound_frames = collections.deque()
+
+
+def _record():
+
+    global sound_frames
+
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 10000
+    RECORD_SECONDS = 5
+    p = pyaudio.PyAudio()
+    
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    while True:
+        data = stream.read(CHUNK)
+        sound_frames.append(data)
+
 
