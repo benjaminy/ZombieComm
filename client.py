c62332f759faf2ef3a5b86939054d37bb1f2428e
evanmega <evan.mega1@gmail.com>
2014-10-19 22:11:52 -0600

    added functionality for user to close server streaming and client

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: Walker Pollard, Jay Batavia, Evan Mega
+# @Date:   2014-10-19 21:20:00
+# @Last Modified by:   evanmega
+# @Last Modified time: 2014-10-19 21:58:07
+
+import pyaudio, os
 from socket import *
-import pyaudio
 
 '''
 ZombieComm radio server
 '''
 
-__author__ = "Walker Pollard, Jay Batavia"
 __version__ = "6.6.6"
 
 
@@ -16,7 +21,7 @@ CHANNELS = 2
 RATE = 10000
 RECORD_SECONDS = 5
 p = pyaudio.PyAudio()
-serverName = '10.0.0.36'
+serverName = ''
 serverPort = 62400
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -26,14 +31,20 @@ Opens socket to zombie radio server and waits for sound
 
 try:
         clientSocket.connect((serverName,serverPort))
+        print "Successfully connected"
 except:
         print "Cannot connnect to server..."
-        
+        os._exit(1)
+
 stream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, output = True)
 data = clientSocket.recv(CHUNK)
 
 while data != ' ':
+    i = raw_input('[q] to terminate client\n')
+    if i == 'q':
+        clientSocket.close()
+        os._exit(1)
 	stream.write(data)
 	data = clientSocket.recv(CHUNK)
-	
+
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,35 +1,38 @@
-import pyaudio
-import collections
-import thread
+# -*- coding: utf-8 -*-
+# @Author: Walker Pollard, Jay Batavia, Evan Mega
+# @Date:   2014-10-19 21:20:00
+# @Last Modified by:   evanmega
+# @Last Modified time: 2014-10-19 22:10:20
+
+import pyaudio, collections, thread, os
 from socket import *
 
 '''
 ZombieComm radio server
 '''
 
-__author__ = "Walker Pollard, Jay Batavia"
 __version__ = "6.6.6"
 
 
 
 sound_frames = collections.deque()
-HOST = '10.0.0.54'
+HOST = ''
 PORT = 62400
 CHUNK = 1024
 
 
 
 def broadCastHandler(connectionSocket, addr):
-    
+
     '''
     Sends chunks from queue through sockets
     '''
-    
+
     global sound_frames
 
     while True:
         try:
-            if len(sound_frames) > 0 and len(sound_frames)<=20: 
+            if len(sound_frames) > 0 and len(sound_frames)<=20:
                 connectionSocket.send(sound_frames.popleft())
             elif len(sound_frames)>20:
                 sound_frames.popleft()
@@ -39,7 +42,7 @@ def broadCastHandler(connectionSocket, addr):
 
 
 def record():
-    
+
     '''
     Records audio from mic in kb chunks
     '''
@@ -51,7 +54,7 @@ def record():
     CHANNELS = 2
     RATE = 10000
     p = pyaudio.PyAudio()
-    
+
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
@@ -59,31 +62,37 @@ def record():
                     frames_per_buffer=CHUNK)
 
     while True:
+        i = raw_input('[q] to terminate stream\n')
+        if i == 'q':
+            stream.close()
+            os._exit(1)
+
         data = stream.read(CHUNK)
         sound_frames.append(data)
 
 
 
 if __name__ == '__main__':
-    
+
     '''
-    Main method handles sockets and 
+    Main method handles sockets and
     calls record, broadcast
     '''
-    
+
     serverPort = PORT
     serverSocket = socket(AF_INET,SOCK_STREAM)
     serverSocket.bind((HOST,serverPort))
     serverSocket.listen(1)
-    
+
     print 'Server is recording'
     thread.start_new_thread(record, ())
-   
+
     while 1:
         try:
             connectionSocket, addr = serverSocket.accept()
             print "Creating new thread"
-            thread.start_new_thread(broadCastHandler, (connectionSocket, addr)) 
+            thread.start_new_thread(broadCastHandler, (connectionSocket, addr))
         except:
+            os._exit(1)
             print "Cannot initialize socket..."
-            break
+
