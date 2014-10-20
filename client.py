9fd5539040e890a1f1c2ee89048a7678f849609a
walker <williamwalkerpollard@gmail.com>
2014-10-19 19:25:38 -0600

    bare-bones working version

--- client.py
+++ client.py
@@ -1,1 +1,25 @@
+from socket import *
+import pyaudio
 
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 10000
+RECORD_SECONDS = 5
+p = pyaudio.PyAudio()
+
+serverName = '10.0.0.36'
+serverPort = 62400
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+
+stream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, output = True)
+
+data = clientSocket.recv(CHUNK)
+
+while data != ' ':
+	stream.write(data)
+	data = clientSocket.recv(CHUNK)
+	
+clientSocket.close()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import pyaudio
 import collections
-import threading
+import thread
+from socket import *
 
 '''
 ZombieComm radio server
@@ -8,6 +9,21 @@ ZombieComm radio server
 
 sound_frames = collections.deque()
 
+
+HOST = '10.0.0.54'
+PORT = 62400
+CHUNK = 1024
+
+def broadCastHandler(connectionSocket, addr):
+    global sound_frames
+
+    while True:
+        try:
+            if len(sound_frames) > 0: 
+                connectionSocket.send(sound_frames.popleft())
+        except:
+            pass
+	connectionSocket.close()
 
 def record():
 
@@ -28,22 +44,24 @@ def record():
     while True:
         data = stream.read(CHUNK)
         sound_frames.append(data)
-
+        #print("hi")
 
 if __name__ == '__main__':
     '''
     main method simply prints recorded data right now
     '''
-
-    t = threading.Thread(target=record)
-    t.start()
-    while True:
-        try:
-            if len(sound_frames) > 0:
-                print (sound_frames.popleft())
-        except:
-            pass
-
-
-
+    serverPort = PORT
+    serverSocket = socket(AF_INET,SOCK_STREAM)
+    serverSocket.bind((HOST,serverPort))
+    serverSocket.listen(1)
+    
+    print 'Server is recording'
+    thread.start_new_thread(record, ())
+    
+    
+    while 1:
+        print "in while loop..."
+        connectionSocket, addr = serverSocket.accept()
+        print "Creating new thread"
+        thread.start_new_thread(broadCastHandler, (connectionSocket, addr)) 
 
