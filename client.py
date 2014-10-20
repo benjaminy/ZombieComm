6c05fb1ec355901296be1c43b09100186d311e88
jay-batavia <bataviajay@gmail.com>
2014-10-19 20:13:02 -0600

    Cleanup and comments

--- client.py
+++ client.py
@@ -1,6 +1,14 @@
 from socket import *
 import pyaudio
 
+'''
+ZombieComm radio server
+'''
+
+__author__ = "Walker Pollard, Jay Batavia"
+__version__ = "6.6.6"
+
+
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -8,14 +16,20 @@ CHANNELS = 2
 RATE = 10000
 RECORD_SECONDS = 5
 p = pyaudio.PyAudio()
-
 serverName = '10.0.0.36'
 serverPort = 62400
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
 
+'''
+Opens socket to zombie radio server and waits for sound
+'''
+
+try:
+        clientSocket.connect((serverName,serverPort))
+except:
+        print "Cannot connnect to server..."
+        
 stream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, output = True)
-
 data = clientSocket.recv(CHUNK)
 
 while data != ' ':
